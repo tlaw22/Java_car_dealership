92e6981973f50513513393aa4cbad78ec2a9ab35
tlaw22 <timothy.j.lawless@youtlook.com>
2024-01-03 09:50:04 -0500

    first commit

---
+++ src/Customer.java
@@ -0,0 +1,8 @@
+public class Customer {
+    String name;
+    String address;
+    double cashOnHand;
+    public void purchaseCar(Vehicle vehicle, Employee emp, boolean finance){
+        Customer cust1 = new Customer
+    }
+}
